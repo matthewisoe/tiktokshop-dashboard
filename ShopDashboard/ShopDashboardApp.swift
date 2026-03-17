9ad0f829b37dc80cd466ef9c3c91082632b1bf3b
matthewisoe <irvingsoenarjo@gmail.com>
2026-03-17 21:17:09 +0800

    Initial Commit

---
+++ ShopDashboard/ShopDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopDashboardApp.swift
+//  ShopDashboard
+//
+//  Created by Matt Irv on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ShopDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
